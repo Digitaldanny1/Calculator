d4819224dc2181ad95cb66a3e7bbe62ef62da3a1
Daniel Nwabuisi <nwabuisi2020@gmail.com>
2025-07-30 10:37:33 +0100

    "Noted"

---
+++ hy.py
@@ -0,0 +1,13 @@
+boys=["Ken","Tim","John"]
+print(boys)
+people=["Luke","Funmi","Magnus"]
+print(people)
+people.extend(boys)
+print(people)
+people[2]="Akintola"
+print(people)
+people.remove("Luke")
+del people[0]
+print(people)
+
+song = "Hagazio"
