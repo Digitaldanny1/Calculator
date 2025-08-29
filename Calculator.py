0cfee885e649c66b0a1fe660db81c955c54a8d07
Daniel Nwabuisi <nwabuisi2020@gmail.com>
2025-08-29 09:08:02 +0100

    hello

--- Calculator.py
+++ Calculator.py
@@ -2,25 +2,31 @@ def calculator():
     try:
         # Get input from user
         num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+        num27 = float(input("Enter second number: "))
+        num3 = float(input("Just Stop: "))
         operation = input("Enter operation (+, -, *, /): ")
 
         # Perform calculation based on operation
         if operation == '+':
-            result = num1 + num2
-            print(f"{num1} + {num2} = {result}")
+            result = num1 + num27
+            print(f"{num1} + {num27} = {result}")
+            print("You Should have stopped when i told you to")
         elif operation == '-':
-            result = num1 - num2
-            print(f"{num1} - {num2} = {result}")
+            result = num1 - num27
+            print(f"{num1} - {num27} = {result}")
+            print("You Should have stopped when i told you to")
         elif operation == '*':
-            result = num1 * num2
-            print(f"{num1} * {num2} = {result}")
+            result = num1 * num27
+            print(f"{num1} * {num27} = {result}")
+            print("You Should have stopped when i told you to")
         elif operation == '/':
-            if num2 == 0:
+            if num27 == 0:
                 print("Error: Cannot divide by zero!")
             else:
-                result = num1 / num2
+                result = num1 / num27
                 print(f"{num1} / {num2} = {result}")
+                print("You Should have stopped when i told you to")
+
         else:
             print("Error: Invalid operation! Please use +, -, *, or /")
             
